14d088c2c76403c48aa42b15b4035b31eb419e1c
Anieb22 <anielloverdino@gmail.com>
2023-07-26 21:35:50 +0200

    ho dimenticato di committare questi file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    Lista fumetti
+   <div class="container">
+    <div class="row">
+        <div class="col-12 d-flex flex-row -justify-content-between flex-wrap">
+            @foreach($comics as $comic)
+            <div class="col-4">
+                <div class="card my-5" style="width: 18rem;">
+                  <img src="{{$comic ->thumb}}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <p class="card-text title" id="title">
+                        {{$comic -> title}}
+                    </p>
+
+                    <p class="card-text" id="body">
+                        {{$comic -> description}}
+                    </p>
+
+                    <h5>Art by</h5>
+                    <p class="card-text" id="body">
+                        {{$comic -> artists}}
+                    </p>
+                    
+                    <h5>Writers</h5>
+                    <p class="card-text" id="body">
+                        {{$comic -> writers}}
+                    </p>
+
+                  </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+   </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
 
     <!-- Styles -->
     @vite('resources/js/app.js')
