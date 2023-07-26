e5dd12bc92f7fc077b53585c7b3f17134b1392f5
Anieb22 <anielloverdino@gmail.com>
2023-07-26 21:03:53 +0200

    collegato database al progetto

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Comic;
+
 class ComicsController extends Controller
 {
-    //
+    public function index(){
+        $comics = Comic::all();
+        dd($comics);
+        return view('home', compact('comics'));
+    }
 }
